28ef611b703eb45ce8a1753179145fb85e8e5fd3
amene815 <amene815@gmail.com>
2019-11-26 13:06:36 -0500

    Oops, I forgot to commit for a while. Adding a bunch of functionality (You can select a card and play said card, you can draw a new card, there is 'fancy' styling on the cards on hover). The majority of the code still needs comments, and there isn't any functionality for interfacing with the rest of the project yet.

--- front-end/game.js
+++ front-end/game.js
@@ -1,22 +1,82 @@
+var card_num = 0;
+
+"use strict";
+
 $(function(){
   $("img").mouseenter(function(){
-      $(this).css('border', '3px solid black');
+    if(has_selected_card()){
+      being_hovered($(this), 1);
+    }
   });
   $("img").mouseleave(function(){
-    $(this).css('border-width', '0');
+    if(has_selected_card()){
+      being_hovered($(this), 0);
+    }
   });
-  $("img").click(function(){
-    $(this).animate({
-      marginLeft: "+=400px",
-      marginTop: "-300px"
-    },1000,function(){
-    });
+  $("img").dblclick(function(){
+    if(has_selected_card()){
+      selected_id = $(this).attr('id')
+      var card = $(this).attr('src');
+      card_num = $(this).attr('alt');
+      var style = $(this).attr('Style').replace(/ /g,'');
+      $("#selected_card").attr({src: card,
+                                alt : card_num,
+                                Style : style + "border:3px solid black",
+                                hidden: false});
+      $(this).attr("hidden", "true");
+      var selected = $("#selected_card");
+      selected.css('border', '3px solid black');
+      $(selected).animate({
+        left: "400",
+        bottom: "+=300"
+        // width: "50px;"
+      },500,function(){
+      });
+    }
   });
 });
 
+function being_hovered(pic, x){
+  if(x){
+    pic.css({'border':'3px solid black', 'z-index': '1','width':'270', 'height':'400'});
+  } else {
+    pic.css({'border':'0px solid black', 'z-index': '-1','width':'200', 'height':'300'});
+  }
+}
+
+function has_selected_card(){
+  return $("#selected_card").attr("hidden");
+}
+
+$(function(){
+  $("#selected_card").click(function(){
+    var prev_card = $(`#${selected_id}`);
+    var left = prev_card.attr("Style").match(/left: (\d+)/)[1]
+    $(this).animate({
+      left: left,
+      bottom: "-=300",
+      height: "300",
+      width: "200"
+    },500,function(){
+      prev_card.attr({hidden: false});
+      being_hovered(prev_card, 0);
+      $(this).attr('hidden','true');
+      });
+  })
+})
+
+
+
+
+$(function(){
+  $("#play").click(function(){
+    //get the information about this card and push it to the server as an event
+    $("#selected_card").attr("hidden", true);
+  })
+})
 $(function(){
   $("#draw").click(function(){
-    card_num++;
-    $("body").append(`<img src = "test_pic1.jpg" alt = "a hand of cards" Style = "height:300px; width: 200px; top:500px; left:${card_num*400}px")>`)
+    // Get new card info and put the new pic in essentially
+    if(has_selected_card()){if(being_hovered($(`#${selected_id}`).attr("hidden", false)));}
   })
 })
