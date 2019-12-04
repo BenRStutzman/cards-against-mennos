0a1a4d8405c83439949402011e3bcea8ea5f7c03
BenRStutzman <tysorx89@gmail.com>
2019-12-04 10:56:47 -0500

    hopefully works without installing flask

--- jp-communication/RUN_ME.py
+++ jp-communication/RUN_ME.py
@@ -1,21 +1,31 @@
 import sys, os, threading, time
-from app import read_and_reset
 
 ## THIS IS THE KEY FUNCTION
 def exchange(message):
     # Sends "message" to the browser and waits for a response, then returns it
-    f = open('py_to_js.txt', 'w')
+    f = open('Lib\\site-packages\\py_to_js.txt', 'w')
     f.write(message)
     f.close()
-    response = read_and_reset('js_to_py.txt')
+    response = read_and_reset('Lib\\site-packages\\js_to_py.txt')
     return response
+
+def read_and_reset(filename):
+    while True:
+        f = open(filename, 'r+')
+        message = f.readline().strip()
+        if message:
+            break
+        f.close()
+    f.truncate(0)
+    f.close()
+    return message
 
 def test_with_terminal():
     while True:
         response = exchange(input('message to send to browser: '))
         print('browser says:', response)
 
-server = threading.Thread(target = os.system, args = {'flask run'})
+server = threading.Thread(target = os.system, args = {'"Lib\\site-packages\\flask" run'})
 server.start()
 
 print("starting server...")
