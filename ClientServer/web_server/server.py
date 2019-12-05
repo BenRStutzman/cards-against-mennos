921c472cad95d3680478fb2459c8d1f9f6f068bd
amene815 <amene815@gmail.com>
2019-12-05 00:57:46 -0500

    Did some more playing around, and I think I found a way to get 'get' requests from the webpage to tell the web server what it should do for the client

--- ClientServer/web_server/server.py
+++ ClientServer/web_server/server.py
@@ -9,6 +9,13 @@ from response.templateHandler import TemplateHandler
 from response.badRequestHandler import BadRequestHandler
 from response.staticHandler import StaticHandler
 
+# how to search several things -- tokenize? Possibly put this stuff into a convenience method
+# basic structure of the url: root/#room/#user/#command/#options where # indicates a variable
+# the next goal is to find a way to put in cookies/login info to track users
+regex = []
+regex.append(re.compile("/(card)(\d)")) #to play a card
+regex.append(re.compile("/(draw)(\d)")) #to draw \d cards not implemented in js yet
+i = 0
 
 class Server(BaseHTTPRequestHandler):
     def do_HEAD(self):
@@ -19,16 +26,19 @@ class Server(BaseHTTPRequestHandler):
         request_extension = split_path[1]
         submitted = split_path[0]
 
+
         if request_extension is "" or request_extension is ".html":
             if self.path in routes:
                 handler = TemplateHandler()
                 handler.find(routes[self.path])
             else:
-                regex = re.compile("word\d")
-                if(regex.match(submtted)):
-                    print("True")
-                else:
-                    handler = BadRequestHandler()
+                for pattern in regex:
+                    match=pattern.match(submitted)
+                    if(match):
+                        handler = [match[1],match[2]]
+                        break
+                    else:
+                        handler = BadRequestHandler()
         elif request_extension is ".py":
             handler = BadRequestHandler()
         else:
@@ -39,9 +49,6 @@ class Server(BaseHTTPRequestHandler):
             'handler': handler
         })
 
-    # def do_POST(self):
-    #     print('Hello World!')
-    #     print(self)
 
     def handle_http(self, handler):
         status_code = handler.getStatus()
@@ -62,5 +69,14 @@ class Server(BaseHTTPRequestHandler):
         return bytes(content, 'UTF-8')
 
     def respond(self, opts):
-        response = self.handle_http(opts['handler'])
-        self.wfile.write(response)
+        try:
+            if(opts['handler'][0] == "card"):
+                global i
+                i+=1
+            # need to send info to the server, which can be done from here?
+
+            # this also currently runs 10 times... not sure why problably something about the unreliablility of the internet and needing to make sure the signal goes through
+                print("Hello World!",i)
+        except:
+            response = self.handle_http(opts['handler'])
+            self.wfile.write(response)
